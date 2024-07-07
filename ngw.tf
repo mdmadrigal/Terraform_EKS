ebf91f76463340a6f4bdf8a87c8c88b0436daf1b
mauricio <mauriciodm@gmail.com>
2024-07-07 10:26:07 -0300

    NatGateway

--- ngw.tf
+++ ngw.tf
@@ -30,7 +30,6 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
   )
 }
 
-
 resource "aws_nat_gateway" "eks_ngw_1b" {
   allocation_id = aws_eip.eks_ngw_eip_1b.id
   subnet_id     = aws_subnet.eks_subnet_public_1b.id
@@ -39,6 +38,39 @@ resource "aws_nat_gateway" "eks_ngw_1b" {
     local.tags,
     {
       Name = "${var.project_name}-ngw_1b"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1a.id
+
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table_1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-priv-route-table_1b"
     }
   )
 }
