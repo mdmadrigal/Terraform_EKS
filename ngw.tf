388e5db68a91b351c01c3c49491fa8a4ac0ff931
mauricio <mauriciodm@gmail.com>
2024-07-07 01:17:59 -0300

    Create ngw.tf

---
+++ ngw.tf
@@ -0,0 +1,44 @@
+resource "aws_eip" "eks_ngw_eip_1a" {
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip_1a"
+    }
+  )
+}
+
+resource "aws_eip" "eks_ngw_eip_1b" {
+  domain = "vpc"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-eip_1b"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "eks_ngw_1a" {
+  allocation_id = aws_eip.eks_ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_public_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw_1a"
+    }
+  )
+}
+
+
+resource "aws_nat_gateway" "eks_ngw_1b" {
+  allocation_id = aws_eip.eks_ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_public_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw_1b"
+    }
+  )
+}
